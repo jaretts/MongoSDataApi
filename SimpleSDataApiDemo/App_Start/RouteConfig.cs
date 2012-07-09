81f0a4a8cc8c1bd27877d04d1534020e65367f8d
Jarett Smith <jaretts@gmail.com>
2012-07-09 11:09:31 -0400

    Removed routes not required by Argos, fixed bugs
    1) Removed routes that did not start with /-/-/-/
    
    2) Re-introduced the action = for SData Collections route. The previous
    commit removed this but that causes a bug where a Get Collection call
    $template. I don't think we do a POSTs on collection anyway

--- SimpleSDataApiDemo/App_Start/RouteConfig.cs
+++ SimpleSDataApiDemo/App_Start/RouteConfig.cs
@@ -14,31 +14,18 @@ namespace SimpleSDataApiDemo
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapHttpRoute("SDataTemplateWithWhere", "sdata/{controller}/$template/query",
-                new { action = "GetTemplate", query = RouteParameter.Optional },
-                new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) });
-
             routes.MapHttpRoute("SDataTemplate", "sdata/-/-/-/{controller}/$template",
                             new { action = "GetTemplate" },
                             new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) });
-
-            routes.MapHttpRoute(name: "SDataSingleResourceKind",
-                 routeTemplate: "sdata/{controller}('{selector}')/{query}",
-                 defaults: new { action = "GetSingle", selector = RouteParameter.Optional,
-                                                        query = RouteParameter.Optional });
 
             routes.MapHttpRoute(name: "SDataSingleResourceKindFull",
                  routeTemplate: "sdata/-/-/-/{controller}('{selector}')/{query}",
                  defaults: new { selector = RouteParameter.Optional,
                                  query = RouteParameter.Optional });
 
-            routes.MapHttpRoute(name: "SDataCollection",
-                routeTemplate: "sdata/{controller}/{query}",
-                defaults: new { action = "GetCollection", query = RouteParameter.Optional });
-
             routes.MapHttpRoute(name: "SDataCollectionFull",
                 routeTemplate: "sdata/-/-/-/{controller}/{query}",
-                defaults: new { query = RouteParameter.Optional });
+                defaults: new { action = "GetCollection", query = RouteParameter.Optional });
 
             routes.MapRoute(
                 name: "Default",
